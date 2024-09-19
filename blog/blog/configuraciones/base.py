418d0466c4180be43c8b76ab5e79284f41b52894
cesarobregon <cesarobregon66@gmail.com>
2024-09-19 16:22:40 -0300

    cambios 19/09

--- blog/blog/configuraciones/base.py
+++ blog/blog/configuraciones/base.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'es_ar'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = ' ‘America/Argentina/Buenos_Aires'
+TIME_ZONE =  'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
